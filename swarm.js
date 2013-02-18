861c344a8875472987e165d9bf6b638642c324d3
Dave Clayton <davedx@gmail.com>
2013-02-18 11:25:49 +0100

    Added button presses (and therefore form submissions) to requirements DSL. Added multiple content match checks.

--- swarm.js
+++ swarm.js
@@ -14,10 +14,11 @@ this.Parser = function(text) {
 		var done = false;
 		var state_location_exp = /(Given|And) (I am at the) '([^']+)' (.+)/;
 		var action_input_exp = /(When I|Then I) (select|fill) '([^']+)' (from the|in the) '([^']+)' (menu|field)/;
+		var click_input_exp = /(When I|Then I) (click the) '([^']+)' button/;
 		var outcome_exp = /(Then the) (.+)/;
 		var outcome_data_exp = /([^:]+): (.+)/;
 		var a = null;
-		var test = { "state": [], "action": [], "outcome": {} };
+		var test = { "state": [], "action": [], "outcome": {}, "match": [] };
 		do {
 			var line = this.lines[this.i++];
 			if(line == '' || line === undefined) {
@@ -27,14 +28,15 @@ this.Parser = function(text) {
 				test.state[a[4]] = a[3];
 			} else if((a = line.match(action_input_exp)) !== null) {
 				test.action.push({ "fn": a[2], "params": [ a[5], a[3] ]});
+			} else if((a = line.match(click_input_exp)) !== null) {
+				test.action.push({ "fn": 'pressButton', "params": [ a[3] ]});
 			} else if((a = line.match(outcome_exp)) !== null) {
 				test.outcome.description = a[2];
 			} else if((a = line.match(outcome_data_exp)) !== null) {
-				test.outcome.class = a[1];
-				test.outcome.text = a[2];
+				test.match.push({ "selector": a[1], "text": a[2] });
 			}
 		} while(!done);
-		if(test.outcome.text !== undefined) {
+		if(test.outcome.description !== undefined) {
 			this.tests.push(test);
 			return true;
 		}
@@ -57,16 +59,30 @@ this.Runner = function(browser, base_url) {
 						browser.visit(base_url+url, function(err) {
 							expect(browser.success).toBe(true);
 
-							var params = tests[n].action[0]["params"];
-							var fn = tests[n].action[0]["fn"];
-							//console.log('fn: '+fn+' params: '+params);
-							params.push(function() {
-								//console.log('browser text = '+browser.text('.'+tests[n].outcome.class));
-								expect(browser.success).toBe(true);
-								expect(browser.text('.'+tests[n].outcome.class)).toContain(tests[n].outcome.text);
-								asyncSpecDone();
-							});
-							browser[fn].apply(browser, params);
+							for(var j=0; j<tests[n].action.length; j++) {
+
+								var params = tests[n].action[j]["params"];
+								var fn = tests[n].action[j]["fn"];
+
+								//console.log('fn: '+fn+' params: '+params);
+								if(j == tests[n].action.length-1) {
+									params.push(function() {
+										//console.log('browser text = '+browser.text('.'+tests[n].outcome.class));
+										expect(browser.success).toBe(true);
+
+										// test matches
+										for(var k=0; k<tests[n].match.length; k++) {
+											var selector = tests[n].match[k].selector;
+											var text = tests[n].match[k].text;
+											console.log('testing selector: '+selector+" vs "+text);
+											expect(browser.text(selector)).toContain(text);
+										}
+
+										asyncSpecDone();
+									});
+								}
+								browser[fn].apply(browser, params);
+							}
 						});
 						asyncSpecWait();
 					});
